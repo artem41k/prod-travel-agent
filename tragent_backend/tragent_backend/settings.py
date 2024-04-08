2374e4bd8a21a99669ed58441a86e0f953d6a47c
artem41k <waw0905@mail.ru>
2024-04-08 23:17:55 +0500

    change default permission class to IsAuthenticated

--- tragent_backend/tragent_backend/settings.py
+++ tragent_backend/tragent_backend/settings.py
@@ -104,7 +104,7 @@ AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'tragent_backend.authentication.TelegramIdSecureAuthentication',
